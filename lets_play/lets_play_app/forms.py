928fd2c190cb4d505944cf83ff3e21849ad2e12d
buhaay <adrian.kodjasz@gmail.com>
2018-02-28 16:28:16 +0100

    correct form choices

--- lets_play/lets_play_app/forms.py
+++ lets_play/lets_play_app/forms.py
@@ -1,11 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 from .models import SportCenter, MyUser
-from lets_play_app.models import Rooms
-from datetimepicker.widgets import DateTimePicker
-
-SPORT_CENTRES = SportCenter.objects.all()
 
 
 class SignUpForm(UserCreationForm):
@@ -19,6 +14,6 @@ class SignUpForm(UserCreationForm):
 
 
 class CreateReservationForm(forms.Form):
-    location = forms.ChoiceField(choices=((center.id, center.name) for center in SPORT_CENTRES),
-                                 label='Wybierz lokalizację')
-    date = forms.DateTimeField(label='Wybierz datę', widget=DateTimePicker())
+    location = forms.ModelChoiceField(queryset=SportCenter.objects.all(),
+                                      empty_label='Wybierz lokalizację')
+    date = forms.DateTimeField(label='Wybierz datę')
