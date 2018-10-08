cdedecdabdfb446504e1f0fafe6d3ba6b4498943
Adrian <adrian.kodjasz@gmail.com>
2018-10-08 19:41:57 +0200

    settings.py - changed redirect url after login

--- lets_play/lets_play/settings.py
+++ lets_play/lets_play/settings.py
@@ -123,7 +123,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-LOGIN_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 MEDIA_URL = '/media/'
